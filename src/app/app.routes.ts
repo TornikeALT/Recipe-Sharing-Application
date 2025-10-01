49de2cc831217a835388690779386a02ab616bd9
TornikeALT <tornikealtunashvili@yahoo.com>
2025-10-01 17:28:14 +0400

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { RecipeListComponent } from '../pages/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from '../pages/recipe-details/recipe-details.component';
+import { RecipeFormComponent } from '../pages/recipe-form/recipe-form.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RecipeListComponent },
+  { path: 'recipe-details/:id', component: RecipeDetailsComponent },
+  { path: 'recipe-form', component: RecipeFormComponent },
+  { path: '**', component: NotFoundComponent },
+];
