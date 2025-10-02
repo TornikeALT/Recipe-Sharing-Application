a776b8d8e4fa0f79ecf645d31741668350b79810
TornikeALT <tornikealtunashvili@yahoo.com>
2025-10-02 21:40:57 +0400

    add details page
    added testing styles too

--- src/components/recipe/recipe.component.ts
+++ src/components/recipe/recipe.component.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
 import { CommonModule } from '@angular/common';
 import { RecipeService } from '../../services/recipe.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css',
 })
 export class RecipeComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
     this.recipeService.getRecipes().subscribe((data) => {
       this.recipes = data;
     });
