d794702e5440142590dc128cd1efeaf880e6161b
TornikeALT <tornikealtunashvili@yahoo.com>
2025-10-03 18:29:35 +0400

    add delete recipe func in details component

--- src/components/details/details.component.ts
+++ src/components/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../../interfaces/recipe';
 import { CommonModule } from '@angular/common';
 
@@ -11,13 +11,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './details.component.css',
 })
 export class DetailsComponent implements OnInit {
+  recipes: Recipe[] = [];
   recipe!: Recipe;
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
+  handleDeleteRecipe(id: string): void {
+    if (confirm('Are you sure you want to delete this recipe?')) {
+      this.recipeService.deleteRecipe(id).subscribe(() => {
+        this.recipes = this.recipes.filter((recipe) => recipe.id !== id);
+        this.router.navigate(['/']);
+      });
+    }
+  }
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.recipeService.getRecipeById(id).subscribe((data: Recipe) => {
